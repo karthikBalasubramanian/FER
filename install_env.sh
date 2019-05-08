866b075c22820ceae836f0f093c332a3ce30dffc
kabalasu <kabalasu@kbalasus-MacBook-Pro.local>
2019-05-07 17:13:31 -0700

    adding env setup and updated proposal

---
+++ install_env.sh
@@ -0,0 +1,16 @@
+# prerequisite have anaconda installed
+# anaconda installation instructions - https://docs.anaconda.com/anaconda/install/
+conda info --envs
+conda create -n fer python=3.6
+conda activate fer
+conda install ipykernel
+python -m ipykernel install --user --name=fer
+conda update conda
+conda install jupyter
+conda install scipy
+conda install scikit-learn
+conda install scikit
+conda install sklearn
+conda install scikit-learn
+conda install keras
+conda install pandas
